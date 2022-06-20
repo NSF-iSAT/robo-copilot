e7b05377645d4abb2f49d0800648b26be8cfdf21
kalebishop <kaleb.bishop@colorado.edu>
2022-06-20 15:02:58 -0600

    added payload field to error message for gdbmi

---
+++ assets/test2.cpp
@@ -0,0 +1,31 @@
+// C++ program to illustrate runtime
+// error by un-assigned variables
+#include <iostream>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    int N = 3;
+  
+    int arr[N];
+  
+    for (int i = 0; i < N; i++) {
+        arr[i] = i;
+    }
+  
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+  
+    return 0;
+}
+
+
+
+
+
+
+
+
+
