2b93ea70021533e234cc0d121d782c6883f138d1
kalebishop <kaleb.bishop@colorado.edu>
2022-08-31 22:03:10 -0600

    updated task

--- docker-connect.sh
+++ docker-connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker exec -ti $(sudo docker ps -aq --filter ancestor=robo_copilot:latest --filter status=running) bash
+docker exec -ti $(docker ps -aq --filter ancestor=robo_copilot:latest --filter status=running) bash
--- run-task.sh
+++ run-task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/ros_ws/devel/setup.bash
-cd assets && cp simple_game_task.cpp /data/task_$1.cpp
-featherpad /data/task_$1.cpp &
+cd assets && cp task_current.cpp /data/task_copy.cpp
+featherpad /data/task_copy.cpp &
 roslaunch robo_copilot copilot_setup.launch use_robot:=true robot_ip:=$2 use_tts:=true
